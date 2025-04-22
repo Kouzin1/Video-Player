2951b9b5b7b43df184c3ac77f65489ecafcc94f6
Kouzin1 <samuelakinwunmi2@gmail.com>
2025-04-22 16:13:09 +0100

    Update

--- scripts.js
+++ scripts.js
@@ -22,6 +22,7 @@ function skip() {
 }
 
 function handleRangeUpdate() {
+  video[this.name] = this.value;
   console.log(this.value);
 }
 
@@ -32,3 +33,6 @@ toggle.addEventListener("click", togglePlay);
 
 skipButtons.forEach((button) => button.addEventListener("click", skip));
 ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
